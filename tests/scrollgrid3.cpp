a2b3ef1ca32248b6601fec3fc251337998e2a8f8
Daniel Maturana <dimatura@cmu.edu>
2014-06-19 15:04:01 -0400

    wip

--- tests/scrollgrid3.cpp
+++ tests/scrollgrid3.cpp
@@ -1,9 +1,16 @@
+
 #include <iostream>
+
+#include <boost/foreach.hpp>
+#include <boost/assign/std/vector.hpp>
+
 #include <Eigen/Core>
+
 #include <scrollgrid/scrollgrid3.hpp>
 
 #include <gtest/gtest.h>
 
+using namespace boost::assign;
 using namespace Eigen;
 using namespace ca;
 
@@ -14,12 +21,13 @@ TEST(scrollgrid3, hash_roundtrip1) {
   float res = 0.15;
   ca::ScrollGrid3f grid3( center, dim, res);
 
-  grid_ix_t ix[5] = { -10, -3, 0, 4, 11 };
+  std::vector<grid_ix_t> ix;
+  ix += -10, -3, 0, 4, 11;
 
-  for (grid_ix_t ii=0; ii < 5; ++ii) {
-    for (grid_ix_t jj=0; jj < 5; ++jj) {
-      for (grid_ix_t kk=0; kk < 5; ++kk) {
-        Vec3Ix gix(ix[ii], ix[jj], ix[kk]);
+  BOOST_FOREACH(grid_ix_t i, ix) {
+    BOOST_FOREACH(grid_ix_t j, ix) {
+      BOOST_FOREACH(grid_ix_t k, ix) {
+        Vec3Ix gix(i, j, k);
         uint64_t hix = grid3.grid_to_hash(gix);
         Vec3Ix gix2 = grid3.hash_to_grid(hix);
         EXPECT_EQ(gix, gix2);
@@ -36,11 +44,13 @@ TEST(scrollgrid3, hash_roundtrip2) {
   float res = 0.15;
   ca::ScrollGrid3f grid3( center, dim, res);
 
-  grid_ix_t ix[5] = { -10, -3, 0, 4, 11 };
-  for (grid_ix_t ii=0; ii < 5; ++ii) {
-    for (grid_ix_t jj=0; jj < 5; ++jj) {
-      for (grid_ix_t kk=0; kk < 5; ++kk) {
-        Vec3Ix gix(ix[ii], ix[jj], ix[kk]);
+  std::vector<grid_ix_t> ix;
+  ix += -10, -3, 0, 4, 11;
+
+  BOOST_FOREACH(grid_ix_t i, ix) {
+    BOOST_FOREACH(grid_ix_t j, ix) {
+      BOOST_FOREACH(grid_ix_t k, ix) {
+        Vec3Ix gix(i, j, k);
         uint64_t hix = grid3.grid_to_hash(gix);
         Vec3Ix gix2 = grid3.hash_to_grid(hix);
         EXPECT_EQ(gix, gix2);
@@ -57,11 +67,13 @@ TEST(scrollgrid3, mem_roundtrip1) {
   float res = 0.15;
   ca::ScrollGrid3f grid3( center, dim, res);
 
-  grid_ix_t ix[5] = { -10, -3, 0, 4, 11 };
-  for (grid_ix_t ii=0; ii < 5; ++ii) {
-    for (grid_ix_t jj=0; jj < 5; ++jj) {
-      for (grid_ix_t kk=0; kk < 5; ++kk) {
-        Vec3Ix gix(ix[ii], ix[jj], ix[kk]);
+  std::vector<grid_ix_t> ix;
+  ix += -10, -3, 0, 4, 11;
+
+  BOOST_FOREACH(grid_ix_t i, ix) {
+    BOOST_FOREACH(grid_ix_t j, ix) {
+      BOOST_FOREACH(grid_ix_t k, ix) {
+        Vec3Ix gix(i, j, k);
         mem_ix_t mix = grid3.grid_to_mem_slow(gix);
         Vec3Ix gix2 = grid3.mem_to_grid(mix);
         std::cerr << "gix = " << gix.transpose() << "|" << " gix2 = " << gix2.transpose() << "\n";
@@ -69,6 +81,7 @@ TEST(scrollgrid3, mem_roundtrip1) {
       }
     }
   }
+
 
 }
 
@@ -79,17 +92,20 @@ TEST(scrollgrid3, mem_roundtrip2) {
   float res = 0.15;
   ca::ScrollGrid3f grid3( center, dim, res);
 
-  grid_ix_t ix[5] = { -10, -3, 0, 4, 11 };
-  for (grid_ix_t ii=0; ii < 5; ++ii) {
-    for (grid_ix_t jj=0; jj < 5; ++jj) {
-      for (grid_ix_t kk=0; kk < 5; ++kk) {
-        Vec3Ix gix(ix[ii], ix[jj], ix[kk]);
-        mem_ix_t mix = grid3.grid_to_mem(gix);
+  std::vector<grid_ix_t> ix;
+  ix += -10, -3, 0, 4, 11;
+
+  BOOST_FOREACH(grid_ix_t i, ix) {
+    BOOST_FOREACH(grid_ix_t j, ix) {
+      BOOST_FOREACH(grid_ix_t k, ix) {
+        Vec3Ix gix(i, j, k);
+        mem_ix_t mix = grid3.grid_to_mem_slow(gix);
         Vec3Ix gix2 = grid3.mem_to_grid(mix);
         EXPECT_TRUE( gix.cwiseEqual(gix2).all() );
       }
     }
   }
+
 }
 
 int main(int argc, char *argv[]) {
