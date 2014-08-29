d8813f5b13251fdc37819792b87fa979e33cdeda
Daniel Maturana <dimatura@cmu.edu>
2014-08-29 03:14:01 -0400

    updated box

--- include/scrollgrid/box.hpp
+++ include/scrollgrid/box.hpp
@@ -1,8 +1,7 @@
 #ifndef BOX_HPP_4WFLKG9Q
 #define BOX_HPP_4WFLKG9Q
 
-#include <assert.h>
-
+#include <ros/ros.h>
 #include <Eigen/Core>
 #include <Eigen/Dense>
 
@@ -22,67 +21,68 @@ class Box {
  public:
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
 
-  Box() {
-    center_.setZero();
-    radius_.setZero();
-    bounds_[0].setZero();
-    bounds_[1].setZero();
+  Box() :
+      center_(Vec::Zero()),
+      radius_(Vec::Zero()),
+      bounds_(Vec::Zero(), Vec::Zero())
+  {
   }
 
   Box(const Vec& min_pt,
       const Vec& max_pt) :
       center_( (min_pt+max_pt)/2 ),
-      radius_( (max_pt-min_pt)/2 )
+      radius_( (max_pt-min_pt)/2 ),
+      bounds_( min_pt, max_pt )
   {
-    bounds_[0] = min_pt;
-    bounds_[1] = max_pt;
+    ROS_ASSERT(check_bounds());
   }
 
   Box(const Box& other) :
       center_(other.center_),
-      radius_(other.radius_)
+      radius_(other.radius_),
+      bounds_(other.bounds_)
   {
-    bounds_[0] = other.bounds_[0];
-    bounds_[1] = other.bounds_[1];
+    ROS_ASSERT(check_bounds());
   }
 
   Box& operator=(const Box& other) {
     if (this==&other) { return *this; }
     center_ = other.center_;
     radius_ = other.radius_;
-    bounds_[0] = other.bounds_[0];
-    bounds_[1] = other.bounds_[1];
+    bounds_ = other.bounds_;
     return *this;
   }
 
-  const Vec& min_pt() const { return bounds_[0]; }
+  const Vec& min_pt() const { return bounds_.first; }
 
-  const Vec& max_pt() const { return bounds_[1]; }
+  const Vec& max_pt() const { return bounds_.second; }
 
   const Vec& bound(int ix) const {
-    assert (ix >= 0);
-    assert (ix < 2);
-    return bounds_[ix];
+    ROS_ASSERT (ix >= 0 && ix < 2);
+    if (ix==0) { return bounds_.first; }
+    return bounds_.second;
   }
 
   void set_bound(int ix, const Vec& v) {
-    assert (ix >= 0);
-    assert (ix < 2);
-    bounds_[ix] = v;
-    center_ = (bounds_[0]+bounds_[1])/2;
-    radius_ = (bounds_[0]-bounds_[1])/2;
+    ROS_ASSERT (ix >= 0 && ix < 2);
+    ROS_ASSERT( check_bounds());
+    if (ix==0) {
+      bounds_.first = v;
+    } else {
+      bounds_.second = v;
+    }
   }
 
   void set_min_pt(const Vec& min_pt) {
-    bounds_[0] = min_pt;
-    center_ = (bounds_[0]+bounds_[1])/2;
-    radius_ = (bounds_[0]-bounds_[1])/2;
+    bounds_.first = min_pt;
+    ROS_ASSERT( check_bounds());
+    this->reset_center_radius();
   }
 
   void set_max_pt(const Vec& max_pt) {
-    bounds_[1] = max_pt;
-    center_ = (bounds_[0]+bounds_[1])/2;
-    radius_ = (bounds_[0]-bounds_[1])/2;
+    bounds_.second = max_pt;
+    ROS_ASSERT( check_bounds());
+    this->reset_center_radius();
   }
 
   const Vec& center() const {
@@ -100,19 +100,19 @@ class Box {
 
   void set_radius(const Vec& v) {
     radius_ = v;
-    bounds_[0] = center_ - radius_;
-    bounds_[1] = center_ + radius_;
+    bounds_.first = center_ - radius_;
+    bounds_.second = center_ + radius_;
   }
 
   void translate(const Vec& v) {
     center_ += v;
-    bounds_[0] += v;
-    bounds_[1] += v;
+    bounds_.first += v;
+    bounds_.second += v;
   }
 
   bool contains(const Vec& v) const {
-    return (bounds_[0].array() <= v.array()).all() &&
-           (v.array() <= bounds_[1].array()).all();
+    return (bounds_.first.array() <= v.array()).all() &&
+        (v.array() <= bounds_.second.array()).all();
   }
 
 #if 0
@@ -125,10 +125,23 @@ class Box {
 #endif
 
  private:
+  // call after bounds change
+  void reset_center_radius() {
+    center_ = (bounds_.first+bounds_.second)/2;
+    radius_ = (bounds_.second-bounds_.first)/2;
+  }
+
+  bool check_bounds() {
+    return bounds_.first.x() <= bounds_.second.x() &&
+           bounds_.first.y() <= bounds_.second.y() &&
+           bounds_.first.z() <= bounds_.second.z();
+  }
+
+ private:
 
   Vec center_;
   Vec radius_;
-  Vec bounds_[2];
+  std::pair<Vec, Vec> bounds_;
 };
 
 }
